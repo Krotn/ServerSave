0a85e628ecd8b2c94cd76df319e5fb4ef5fa3163
Karl Otness <karl@otness.com>
2011-06-15 18:48:11 -0500

    ServerSave now stores a LogManager

--- src/me/krotn/ServerSave/ServerSave.java
+++ src/me/krotn/ServerSave/ServerSave.java
@@ -1,13 +1,16 @@
 package me.krotn.ServerSave;
 
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ServerSave extends JavaPlugin{
-	
+	private SSLogManager logMan = new SSLogManager(Logger.getLogger("Minecraft"));
 	public void onEnable(){
 		if(!SSDirectoryManager.directoryExists()){
 			SSDirectoryManager.createDirectory();
 		}
+		logMan.info("ServerSave enabled!");
 	}
 	
 	public void onDisable(){
