fc77498fe01646378df8bf48541940d7e9fca8c7
Karl Otness <karl@otness.com>
2011-06-15 18:34:40 -0500

    Added main plugin class, "ServerSave" to the project.

---
+++ src/me/krotn/ServerSave/ServerSave.java
@@ -0,0 +1,14 @@
+package me.krotn.ServerSave;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ServerSave extends JavaPlugin{
+	
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+}
