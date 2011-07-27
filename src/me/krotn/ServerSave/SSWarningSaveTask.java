be90aaec4c9112889d2eef36f7d0b3ac55338d79
Karl Otness <karl@otness.com>
2011-07-27 10:18:45 -0500

    Added SSWarningSaveTask to the project.
    This class will handle scheduling the saves after giving warnings.

---
+++ src/me/krotn/ServerSave/SSWarningSaveTask.java
@@ -0,0 +1,23 @@
+package me.krotn.ServerSave;
+
+public class SSWarningSaveTask implements Runnable{
+	private ServerSave plugin = null;
+	private SSPropertiesManager propMan = null;
+	private SSTaskManager taskMan = null;
+	private ServerSaveTask saveTask = null;
+	private long saveTime = 600;
+	private long warningTime = 30;
+	
+	public SSWarningSaveTask(ServerSave plugin,ServerSaveTask saveTask,long saveTime,long warningTime){
+		this.plugin = plugin;
+		this.propMan = plugin.getPropertiesManager();
+		this.taskMan = plugin.getTaskManager();
+		this.saveTask = saveTask;
+		this.saveTime = saveTime;
+		this.warningTime = warningTime;
+	}
+	
+	public void run(){
+		
+	}
+}
